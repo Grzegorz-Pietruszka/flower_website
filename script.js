8761b472106733459f096042bdd75823a5f2e95d
Grzegorz Pietruszka <42655303+Grzegorz-Pietruszka@users.noreply.github.com>
2019-04-18 11:56:07 +0100

    added preload images

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
+// preload background images
+$.preload('img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg', 'img/img4.jpg');
+
 //menu icon animation
-$("#wrapper").click( function() {
-	$(".menu").toggleClass("close");
+$('#wrapper').click(function() {
+  $('.menu').toggleClass('close');
 });
 
 //logo animation
 $('#info-click').click(function() {
-    if($('#info-par').is(':visible')){
-        $('#logo').css({'transform' : 'rotate(90deg)', });
-    } else {
-        $('#logo').css({'transform' : 'rotate(0deg)', });
-    };         
-   
-    $('#info-par').slideToggle('slow')
+  if ($('#info-par').is(':visible')) {
+    $('#logo').css({
+      transform: 'rotate(90deg)'
     });
+  } else {
+    $('#logo').css({
+      transform: 'rotate(0deg)'
+    });
+  }
+
+  $('#info-par').slideToggle('slow');
+});
 
 //background slider
-var images=['img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg', 'img/img4.jpg', 'img/img5.jpg'];
-var c=0;
-setInterval(function(){
-   c++;
-   if(c==4){
-    c=0;
+var images = ['img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg', 'img/img4.jpg'];
+var c = 0;
+setInterval(function() {
+  c++;
+  if (c == 4) {
+    c = 0;
   }
-  document.body.style.backgroundImage = 'url('+images[c]+')';
-},6000);
+  document.body.style.backgroundImage = 'url(' + images[c] + ')';
+}, 6000);
 
 //toggle menu slidebar
 $('#menu-click').click(function() {
-    $('.toggle-menu').toggleClass('menu-active');
-    if($('.toggle-menu').hasClass('menu-active')) {
-        $('#info-par').slideUp();
-        $('#logo').css('transform', 'rotate(90deg)' );
-    }
+  $('.toggle-menu').toggleClass('menu-active');
+  if ($('.toggle-menu').hasClass('menu-active')) {
+    $('#info-par').slideUp();
+    $('#logo').css('transform', 'rotate(90deg)');
+  }
 });
